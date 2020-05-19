630aed21780effb2e333516ce4ce8cc2e4380925
Azmira Tania <azmiratania@users.noreply.github.com>
2020-05-20 00:24:28 +0800

    Initial commit

---
+++ HangmanFunctions.py
@@ -0,0 +1,266 @@
+#description     : A simple game of hangman. 
+#author          : Azmira Tania
+#date            : 10 November 2012
+#version         : 3.6
+#python_version  : 3.2.3
+#code purpose    : Contains all functions required for Hangman game.
+#==============================================================================
+
+from random import randint
+
+# This function takes an integer input and displays corresponding ASCII art.
+def display_pics(n):
+
+    graphics =["""
+
+   
+        
+        
+        
+         
+         
+  
+ 
+
+""","""
+
+   
+        
+        
+        
+         
+         
+  
+ _____
+
+""","""
+
+   
+  |      
+  |      
+  |       
+  |       
+  |       
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      
+  |       
+  |       
+  |       
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      (_)
+  |       
+  |       
+  |       
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      (_)
+  |       |
+  |       |
+  |       
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      (_)
+  |      \|
+  |       |
+  |       
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      (_)
+  |      \|/
+  |       |
+  |       
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      (_)
+  |      \|/
+  |       |
+  |      / 
+  |
+ _|___
+
+""","""
+
+   _______
+  |/      |
+  |      (_)
+  |      \|/
+  |       |
+  |      / \ GAME OVER
+  |
+ _|___
+
+""","""
+
+
+
+
+
+    (_)
+    \|/
+     |
+    / \ YOU WIN!
+
+"""]
+
+    print(graphics[n])
+
+
+
+
+
+
+
+
+
+
+
+# This function allows the user to choose a category.
+# Upon choosing a category, a random word selected from category list.
+# This function returns the random word and the choosen category name. 
+def choose_category():
+    
+    # Convert string to uppercase and store the words seperated by spaces into the list.
+    animals = 'alligator giraffe monkey hippopotamus chameleon gorilla kitten panda sheep rabbit'.upper().split()
+    fruits = 'apple orange pear banana blackberry mango peach lychee durian watermelon'.upper().split()
+    country = 'Bangladesh China Brazil Singapore India Vietnam Nepal Japan Canada Mexico'.upper().split()
+    flowers = 'rose sunflower jasmine dahlia gardenia poppy daffodil daisy tulip hydrangea'.upper().split()
+
+
+    # Prompt user to choose a category until a valid input is provided. 
+    while 1:
+        # Handle errors on non-numeric inputs.
+        try:
+            category_choice = int(input("1. Animals\n2. Fruits\n3. Country\n4. Flowers\nChoose a category [1-4]: "))
+        except:
+            category_choice = 0
+        if category_choice >= 1 and category_choice <= 4:
+            break
+        else:
+            print("Please enter a choice between 1 and 4.\n")
+
+    # Generate a random word from category based on user's category choice
+    if category_choice == 1:
+        word = animals[randint(0,9)]
+        category_name = "Animals"
+    elif category_choice == 2:
+        word = fruits[randint(0,9)]
+        category_name = "Fruits"
+    elif category_choice == 3:
+        word = country[randint(0,9)]
+        category_name = "Country"
+    elif category_choice == 4:
+        word = flowers[randint(0,9)]
+        category_name = "Flowers"
+        
+
+    print("\nThe category you have choosen is: ",category_name,"\n" )
+
+    # Return a list containing the random word and category name.
+    return [word,category_name]
+
+
+
+
+
+
+
+
+
+
+
+
+# This function takes an integer input and displays corresponding alert messages.
+def alert_message(n):
+    messageList = ["""
+
+-------------------------------------------
+| You have already guessed this character |
+-------------------------------------------
+
+""","""
+
+-------------------------------------------
+|        Character not in the word        |
+-------------------------------------------
+
+""","""
+
+-------------------------------------------
+|       You can only input alphabets      |
+-------------------------------------------
+
+""","""
+
+-------------------------------------------
+|     You guessed the word correctly      |
+-------------------------------------------
+
+""","""
+
+--------------------------------------------------------
+| Please enter either a single character or whole word |
+--------------------------------------------------------
+
+""","""
+
+-----------------------------------------------
+| That is not the correct match! Keep trying! |
+-----------------------------------------------
+
+"""]
+
+    print (messageList[n])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ HangmanV3.py
@@ -0,0 +1,128 @@
+#description     : A simple game of hangman. 
+#author          : Azmira Tania
+#date            : 10 November 2012
+#version         : 3.6
+#python_version  : 3.2.3
+#code purpose    : Contains main Hangman game codes.
+#==============================================================================
+
+
+import time
+
+# Import the HangmanFunctions.py module, which contains the funtions, from the same folder
+import HangmanFunctions
+
+
+# Allow the user to play again
+while 1:
+    # Call choose_category function and store the random word and category name
+    cat_list = HangmanFunctions.choose_category()
+    word = cat_list[0]
+    category_name = cat_list[1]
+
+    word_length = len(word)
+    print("The word to guess is",word_length," characters long")
+
+    # Seperate the individual characters of the random word
+    word_list = list(word)
+
+    # Create a empty list to store correctly guessed words
+    guessed_char = list(word)
+    for i in range(0,word_length):
+        guessed_char[i] = ""
+
+    # An empty list to store previously guessed characers
+    previous_inputs = []
+
+    max_tries = 9
+    tries = 0
+    guess_char = ""
+
+    while 1:
+        # Call ASCII art function with current number of tries
+        HangmanFunctions.display_pics(tries)
+
+        # Check if the user exceeded 9 tries and end the game 
+        if tries >= max_tries:
+            print("YOU FAIL!")
+            print("Correct word: ",word)
+            break
+
+        # Display category name with blank lines or correct guessed characters
+        print(category_name,":", end = " ")
+        for i in range(0, word_length):
+            # Check if currently guessed characters is correct
+            if guess_char == word_list[i]:
+                guessed_char[i] = guess_char
+                print(guessed_char[i],end =" ")
+            # Display correctly guessed previous characters    
+            elif guessed_char[i] == word_list[i]:
+                print(guessed_char[i],end =" ")
+            else:
+                print("_",end =" ")
+        print()
+
+        # Check if all the characters have been correctly guessed
+        if guessed_char == word_list:
+            HangmanFunctions.display_pics(10)
+            break
+
+        # Display all previously guessed characters
+        if len(previous_inputs)> 0:
+            print("Previous guesses: ",end = "")
+            for d in previous_inputs:
+                print(d,end = " ")
+            print()
+
+        # Ask the user for a character    
+        guess_char = input("Please input a character or guess the word: ")
+
+        guess_char = guess_char.upper()
+        input_length = len(guess_char)
+        
+        # Check if user entered a single character
+        if input_length == 1:
+            charnum = ord(guess_char)
+            # Check the character is an alphabet, otherwise display error message
+            if (charnum >= 65 and charnum <=90) or (charnum >= 97 and charnum <= 122):
+                # Check that the character has been previously entered
+                if guess_char in previous_inputs:
+                    HangmanFunctions.alert_message(0)
+                # Check if the user entered a correct character
+                elif guess_char in word_list:
+                    previous_inputs.append(guess_char)
+                # For wrong character input, increment tries by 1 and show error message.
+                else:
+                    tries+=1
+                    previous_inputs.append(guess_char)
+                    HangmanFunctions.alert_message(1)
+            else:
+                HangmanFunctions.alert_message(2)
+        # Check if the user guessed the whole word, otherwise display error
+        elif input_length == word_length:
+            # Check if the whole word was correctly guessed
+            if guess_char == word:
+                HangmanFunctions.alert_message(3)
+                HangmanFunctions.display_pics(10)
+                break
+            else:
+                tries+=1
+                HangmanFunctions.alert_message(5)
+        else:
+            HangmanFunctions.alert_message(4)
+
+    # Prompt the user to play again
+    playAgain = input("Would you like to play again? (Y/N): ")
+    if playAgain == "N" or playAgain == "n" :
+        break
+    else:  
+        print()
+        print()
+        print() 
+
+        
+        
+
+
+
+
